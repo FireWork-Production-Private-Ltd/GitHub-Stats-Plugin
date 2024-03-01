fa089e3641f6ed3fc9df499f08c67e7dd8a1a8ff
Utsav Patel <75293077+up1512001@users.noreply.github.com>
2024-03-01 19:55:32 +0000

    Feature: should break phpcs workflow

---
+++ plugins/github-stats/src/example-file-phpcs-fail.php
@@ -0,0 +1,12 @@
+<?php
+
+class Example_file_phpcs_fail{
+    public function __construct() {
+        echo "Hello World!";
+    }
+
+    public function init(){
+        echo "init method called";
+    }
+
+}
